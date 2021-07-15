c531202eb9e59c471c46b83f16951693d9c8ef87
Charles ROUGE <c.rouge@sheffield.ac.uk>
2021-07-15 22:01:13 +0100

    15 July

--- brouillon.py
+++ brouillon.py
@@ -1,14 +1,16 @@
 from deterministic import deterministic_family
+from ensemble import ensemble_family
 import numpy as np
+import pandas as pd
 
 main_dir = '../../iRONS/iRONS/Notebooks/B - Implementation/Inputs/'
 datafile = main_dir + 'hist_clim_data.csv'
 forecast_folder = main_dir + 'ECMWF forecasts csv/'
-key_forecast_string = '_1d_7m_ECMWF_'
-var_name = 'Temp'
+var_name = ['Rain', 'Rain']
 family_destination = 'families/'
 skill_values = np.linspace(0, 1, 5)
 beg_date = '1981/1/10'
 end_date = '1981/4/12'
 
-deterministic_family(datafile, forecast_folder, key_forecast_string, var_name, family_destination, skill_values, beg_date, end_date, 'MAE')
+#deterministic_family(datafile, forecast_folder, var_name, family_destination, skill_values, beg_date, end_date, 'MSE')
+ensemble_family(datafile, forecast_folder, var_name, family_destination, skill_values, beg_date, end_date)
---
+++ ensemble.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+def ensemble_family(hist_file, forecast_folder, variable_names, family_folder, skill_values, begin_date, end_date):
+
+    """ This function creates families of new ensemble forecasts of desired skills from existing ensemble forecasts.
+        It then saves the resulting forecast families in CSV files: 1 per forecast
+
+        WARNING: this only works for monthly forecasts. For a different time interval between forecast, modify line XX
+
+        inputs = (hist_file, forecast_folder, variable_name, family_folder, skill_values, begin_date, end_date, mase)
+
+        hist_file           = Full path to file with historical data for which we have the forecasts
+        forecast_folder     = Path to the folder where the forecasts are. Enter as string.
+        variable_names      = String duplet with the name of the variable both in historical data and forecasts
+        family_folder       = Folder where to save outputs. Enter as string.
+        skill_values        = 1-D Numpy array with the desired skill values (typically between 0 and 1).
+        begin_date          = date at which we start making families from available forecasts. Format 'YYYY/MM/DD'
+        end_date            = date after which we stop making families from available forecasts. Format 'YYYY/MM/DD'
+
+        No output variable: output printed to file
+    """
+
+    # Read historical data
+    hist_all = pd.read_csv(hist_file, index_col=0)
+    hist_all.index = pd.to_datetime(np.array(hist_all.index), format='%d/%m/%Y')
+
+    # List dates for forecasts to pull
+    date_list = pd.date_range(start=begin_date, end=end_date, freq='MS')
+
+    # Loop on forecast dates
+    for t in date_list:
+
+        # Read forecast data
+        fore_data = pd.read_csv(forecast_folder + '/' + str(10000*t.year + 100*t.month + t.day) + '_1d_7m_ECMWF_' +
+                                variable_names[1] + '.csv', index_col=0)
+        fore_data.index = pd.to_datetime(np.array(fore_data.index), format='%d/%m/%Y')
+
+        # Get the historical data that corresponds to the forecast period
+        hist_data = pd.Series(hist_all.loc[fore_data.index][variable_names[0]])
+
+        # For precipitation and evaporation: historical data needs to be turned into a cumulative sum
+        if variable_names[0] == 'Rain' or variable_names[0] == 'PET':
+            hist_data = hist_data.cumsum()
+
+        # Build forecast family
+        for i in range(len(skill_values)):
+
+            # Compute multiplier k
+            k = 1 - skill_values[i]
+
+            # Member of the forecast family for this skill level
+            family_member = pd.DataFrame(data=None, index=fore_data.index)
+            for j in range(fore_data.shape[1]):
+                family_member.insert(j, column=fore_data.columns[j],
+                                     value=(1 - k) * hist_data.values + k * fore_data.values[:, j])
+
+            skill_folder = family_folder + '/ECMWF_Ensemble_skill_CRPSS=' + str(skill_values[i])
+            if os.path.exists(skill_folder) is False:
+                os.mkdir(skill_folder)
+
+            family_member.to_csv(path_or_buf=skill_folder + '/' + str(10000*t.year + 100*t.month + t.day) +
+                                 '_1d_7m_ECMWF_' + variable_names[1] + '.csv')
+
+    return None
